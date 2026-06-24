c107d55ff422efaa7ba09b24264b413a461495b1
surgeplayer271 <surgeplayer271@gmail.com>
2026-06-24 20:23:04 +1200

    initial dotfiles: niri, waybar, foot, zsh

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias ff='fastfetch'
+# Clear the screen first, print a few blank lines, then run fastfetch
+clear
+ff
